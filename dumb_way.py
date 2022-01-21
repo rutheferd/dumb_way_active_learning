dafaad6f9e83f2fe885e0d3ae7d8bd2dccc5d84c
Austin Ruth <austin.ruth@gtri.gatech.edu>
2022-01-21 10:00:35 -0500

    model

--- dumb_way.py
+++ dumb_way.py
@@ -14,13 +14,14 @@ from tensorflow.keras.models import Sequential
 import glob
 import os
 import random
+import cv2
 
 #%%
 batch_size = 32
 img_height = 480
 img_width = 720
 
-data_dir = ""
+data_dir = "/Users/aruth3/Documents/repos/dumb_way_active_learning/PetImages"
 
 # %%
 train_ds = tf.keras.utils.image_dataset_from_directory(
@@ -108,33 +109,38 @@ with open("model.tflite", "wb") as f:
 # Possible Name: Generate Label Queries
 new_model = tf.keras.models.load_model("latest_model")
 
-unlabel_dir = ""
+unlabel_dir = "/Users/aruth3/Documents/repos/dumb_way_active_learning/Pet_Unlabeled"
 
 # %%
-filelist = glob.glob(unlabel_dir + "/*.png")
+filelist = glob.glob(unlabel_dir + "/*.jpg")
 
 saved_predictions = []
 
+# x = np.array([np.array(cv2.imread(fname)) for fname in filelist])
+
 for i, e in enumerate(filelist):
+    if i >= 10000:
+        break
     img = tf.keras.utils.load_img(e, target_size=(img_height, img_width))
     img_array = tf.keras.utils.img_to_array(img)
+
     img_array = tf.expand_dims(img_array, 0)  # Create a batch
 
     predictions = new_model.predict(img_array)
     score = tf.nn.softmax(predictions[0])
     conf = 100 * np.max(score)
 
-    # print(
-    #     "{}/{} This image most likely belongs to {} with a {:.2f} percent confidence.".format(
-    #         i, len(filelist), class_names[np.argmax(score)], 100 * np.max(score)
-    #     )
-    # )
+    print(
+        "{}/{} This image most likely belongs to {} with a {:.2f} percent confidence.".format(
+            i, len(filelist), class_names[np.argmax(score)], 100 * np.max(score)
+        )
+    )
 
     if conf < 60.0:
         saved_predictions.append(i)
 
 # %%
-temp_to_label = ""
+temp_to_label = "/Users/aruth3/Documents/repos/dumb_way_active_learning/to_label"
 num_label = 5
 please_label = random.choices(saved_predictions, k=num_label)
 for i in please_label:
